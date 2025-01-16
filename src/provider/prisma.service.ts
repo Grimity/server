ee2bfe6abc6ba6b3626e3281ac617f128ff6acc9
leemhoon00 <leemhoon000@gmail.com>
2025-01-16 23:25:45 +0900

    feat: logging prisma info

--- src/provider/prisma.service.ts
+++ src/provider/prisma.service.ts
@@ -2,6 +2,11 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: process.env.NODE_ENV === 'production' ? ['info'] : [],
+    });
+  }
   async onModuleInit() {
     await this.$connect();
   }
