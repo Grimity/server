95b59bf23fa40c9752170bcc1fd4399ec0ffb49d
leemhoon00 <leemhoon000@gmail.com>
2025-01-17 02:02:36 +0900

    docs: 알림 조회 api 명세

--- src/controller/notification.controller.ts
+++ src/controller/notification.controller.ts
@@ -1,15 +1,27 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  ApiTags,
+  ApiResponse,
+  ApiBearerAuth,
+  ApiOperation,
+} from '@nestjs/swagger';
 import { NotificationService } from 'src/provider/notification.service';
 import { JwtGuard } from 'src/common/guard';
 import { CurrentUser } from 'src/common/decorator';
+import { NotificationDto } from './dto/notification';
 
+@ApiBearerAuth()
+@ApiTags('/notifications')
+@ApiResponse({ status: 401, description: 'Unauthorized' })
 @Controller('notifications')
 export class NotificationController {
   constructor(private notificationService: NotificationService) {}
 
+  @ApiOperation({ summary: '알림 목록 조회' })
+  @ApiResponse({ status: 200, type: NotificationDto, isArray: true })
   @Get()
   @UseGuards(JwtGuard)
-  async getAll(@CurrentUser() userId: string) {
+  async getAll(@CurrentUser() userId: string): Promise<NotificationDto[]> {
     return await this.notificationService.getAll(userId);
   }
 }
