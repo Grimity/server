f3c15bfa72dbe354f2071e7dbc4292ea9c9fde26
leemhoon00 <leemhoon000@gmail.com>
2025-01-17 00:53:04 +0900

    feat: 댓글, 대댓글 알림

--- src/provider/feed-comment.service.ts
+++ src/provider/feed-comment.service.ts
@@ -1,12 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { FeedCommentRepository } from 'src/repository/feed-comment.repository';
+import { NotificationRepository } from 'src/repository/notification.repository';
 
 @Injectable()
 export class FeedCommentService {
-  constructor(private feedCommentRepository: FeedCommentRepository) {}
+  constructor(
+    private feedCommentRepository: FeedCommentRepository,
+    private notificationRepository: NotificationRepository,
+  ) {}
 
   async create(userId: string, input: CreateFeedCommentInput) {
-    await this.feedCommentRepository.create(userId, input);
+    const data = await this.feedCommentRepository.create(userId, input);
+
+    if (userId === data.feed.authorId) return;
+
+    const actorId = userId;
+    const refId = input.feedId;
+    const type = 'COMMENT';
+
+    await this.notificationRepository.create({
+      actorId,
+      refId,
+      type,
+      userId: data.feed.authorId,
+    });
+
+    if (
+      !data.parent ||
+      data.parent.writerId === userId ||
+      data.parent.writerId === data.feed.authorId
+    )
+      return;
+
+    await this.notificationRepository.create({
+      actorId,
+      refId,
+      type,
+      userId: data.parent.writerId,
+    });
+
     return;
   }
 
--- src/repository/notification.repository.ts
+++ src/repository/notification.repository.ts
@@ -4,4 +4,17 @@ import { PrismaService } from 'src/provider/prisma.service';
 @Injectable()
 export class NotificationRepository {
   constructor(private prisma: PrismaService) {}
+
+  async create(input: CreateNotificationInput) {
+    await this.prisma.notification.create({
+      data: input,
+    });
+  }
 }
+
+export type CreateNotificationInput = {
+  actorId: string;
+  refId?: string | null;
+  type: string;
+  userId: string;
+};
