97387c9af199edbcf5c5accb8f8a4eb6fd54c3d5
leemhoon00 <leemhoon000@gmail.com>
2025-01-17 20:56:12 +0900

    install sqs, dynamodb

--- src/provider/aws.service.ts
+++ src/provider/aws.service.ts
@@ -3,14 +3,17 @@ import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { ConfigService } from '@nestjs/config';
 import { v4 as uuidv4 } from 'uuid';
+import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs';
 
 @Injectable()
 export class AwsService {
   private configService: ConfigService;
   private s3Client: S3Client;
+  private sqsClient: SQSClient;
   constructor(configService: ConfigService) {
     this.configService = configService;
     this.s3Client = new S3Client();
+    this.sqsClient = new SQSClient();
   }
 
   async getUploadUrl(
@@ -32,4 +35,32 @@ export class AwsService {
     });
     return await getSignedUrl(this.s3Client, command, { expiresIn: 60 });
   }
+
+  async pushEvent(event: LikeEvent | FollowEvent | CommentEvent) {
+    const command = new SendMessageCommand({
+      QueueUrl: this.configService.get('AWS_SQS_URL'),
+      MessageBody: JSON.stringify(event),
+    });
+    await this.sqsClient.send(command);
+    return;
+  }
 }
+
+export type LikeEvent = {
+  type: 'LIKE';
+  actorId: string;
+  feedId: string;
+};
+
+export type FollowEvent = {
+  type: 'FOLLOW';
+  actorId: string;
+  userId: string;
+};
+
+export type CommentEvent = {
+  type: 'COMMENT';
+  actorId: string;
+  feedId: string;
+  parentCommentId?: string | null;
+};
