8238776f90518beadcf4c9b13206ccd5ad88eb0b
leemhoon00 <leemhoon000@gmail.com>
2025-01-18 23:30:37 +0900

    dynamodb 삭제

--- src/repository/notification.repository.ts
+++ src/repository/notification.repository.ts
@@ -1,50 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { ConfigService } from '@nestjs/config';
-import { QueryCommand } from '@aws-sdk/lib-dynamodb';
+import { PrismaService } from 'src/provider/prisma.service';
 
 @Injectable()
 export class NotificationRepository {
-  private dynamoDBClient: DynamoDBClient;
-  constructor(private configService: ConfigService) {
-    this.dynamoDBClient = new DynamoDBClient();
-  }
-
-  async findAll(userId: string) {
-    // createdAt desc
-    const command = new QueryCommand({
-      TableName: this.configService.get('AWS_DYNAMODB_TABLE_NAME'),
-      KeyConditionExpression: 'userId = :userId',
-      ExpressionAttributeValues: {
-        ':userId': userId,
-      },
-      ScanIndexForward: false,
-      Limit: 50,
-    });
-    const response = await this.dynamoDBClient.send(command);
-    const items = response.Items as NotificationItem[];
-    return items.map((item) => {
-      return {
-        feedId: item.feedId || null,
-        createdAt: new Date(item.createdAt),
-        actorId: item.actorId,
-        actorName: item.actorName,
-        id: item.id,
-        isRead: item.isRead,
-        type: item.type,
-      };
-    });
-  }
+  constructor(private prisma: PrismaService) {}
 }
-
-export type NotificationItem = {
-  expiresAt: number;
-  userId: string;
-  feedId?: string;
-  createdAt: number;
-  actorId: string;
-  actorName: string;
-  id: string;
-  isRead: boolean;
-  type: 'LIKE' | 'COMMENT' | 'FOLLOW';
-};
