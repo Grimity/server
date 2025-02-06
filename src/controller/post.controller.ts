08084fb1a46fcb7cc18593e8570db694dcc3db31
leemhoon00 <leemhoon000@gmail.com>
2025-02-06 22:25:33 +0900

    docs: 게시글 생성 api 명세

--- src/controller/post.controller.ts
+++ src/controller/post.controller.ts
@@ -1,13 +1,29 @@
 import { Controller, Post, UseGuards, Body } from '@nestjs/common';
+import {
+  ApiTags,
+  ApiResponse,
+  ApiOperation,
+  ApiBearerAuth,
+} from '@nestjs/swagger';
 import { PostService } from 'src/provider/post.service';
 import { JwtGuard } from 'src/common/guard';
 import { CurrentUser } from 'src/common/decorator';
 import { CreatePostDto, PostIdDto } from './dto/post';
 
+@ApiTags('/posts')
+@ApiResponse({ status: 401, description: 'Unauthorized' })
+@ApiResponse({ status: 400, description: '유효성 검사 실패' })
 @Controller('posts')
 export class PostController {
   constructor(private postService: PostService) {}
 
+  @ApiBearerAuth()
+  @ApiOperation({ summary: '게시글 생성' })
+  @ApiResponse({
+    status: 201,
+    description: '게시글 생성 성공',
+    type: PostIdDto,
+  })
   @Post()
   @UseGuards(JwtGuard)
   async create(
