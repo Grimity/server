38a94cf4a58c02ee3ffd0b9de58972d90426ff60
leemhoon00 <leemhoon000@gmail.com>
2025-02-14 02:53:07 +0900

    feat: prisma-kysely 세팅

--- src/provider/prisma.service.ts
+++ src/provider/prisma.service.ts
@@ -1,13 +1,43 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import {
+  Kysely,
+  PostgresAdapter,
+  PostgresIntrospector,
+  PostgresQueryCompiler,
+} from 'kysely';
+import kyselyExtension from 'prisma-extension-kysely';
+import { DB } from 'prisma/generated/types';
+
+declare module '@prisma/client' {
+  interface PrismaClient {
+    $kysely: Kysely<DB>;
+  }
+}
+
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
     super({
       log: process.env.NODE_ENV === 'production' ? ['info'] : [],
+      // log: ['query', 'info', 'warn'],
     });
   }
   async onModuleInit() {
+    const extension = this.$extends(
+      kyselyExtension({
+        kysely: (driver) =>
+          new Kysely<DB>({
+            dialect: {
+              createDriver: () => driver,
+              createAdapter: () => new PostgresAdapter(),
+              createIntrospector: (db) => new PostgresIntrospector(db),
+              createQueryCompiler: () => new PostgresQueryCompiler(),
+            },
+          }),
+      }),
+    );
+    this.$kysely = extension.$kysely;
     await this.$connect();
   }
 }
