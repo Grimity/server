ca46857c7db00003919cd9a2ccc6f139853442b1
leemhoon00 <leemhoon000@gmail.com>
2025-04-10 17:10:45 +0900

    db: isAI 컬럼 삭제

---
+++ prisma/migrations/20250410080720_remove_is_ai/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isAI` on the `Feed` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Feed" DROP COLUMN "isAI";
