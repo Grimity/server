344b8a3a6290108462343f3c305664d94a4c6ddf
leemhoon00 <leemhoon000@gmail.com>
2025-04-27 20:26:03 +0900

    test: 내 정보 관련 테스트 파일 이동

---
+++ test/e2e/me/delete-me.e2e-spec.ts
@@ -0,0 +1,130 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from 'src/app.module';
+import { PrismaService } from 'src/database/prisma/prisma.service';
+import { AuthService } from 'src/provider/auth.service';
+import { register } from '../helper';
+
+describe('DELETE /me - 회원탈퇴', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+  let authService: AuthService;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = module.createNestApplication();
+    prisma = module.get<PrismaService>(PrismaService);
+    authService = module.get<AuthService>(AuthService);
+
+    jest.spyOn(authService, 'getKakaoProfile').mockResolvedValue({
+      kakaoId: 'test',
+      email: 'test@test.com',
+    });
+
+    await app.init();
+  });
+
+  afterEach(async () => {
+    await prisma.user.deleteMany();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('accessToken이 없을 때 401을 반환한다', async () => {
+    // when
+    const { status } = await request(app.getHttpServer()).delete('/me').send();
+
+    // then
+    expect(status).toBe(401);
+  });
+
+  it('204와 함께 모든 데이터를 삭제하지만 postComment는 남긴다', async () => {
+    // given
+    const accessToken = await register(app, 'test');
+
+    const [user, user2] = await Promise.all([
+      prisma.user.findFirstOrThrow(),
+      prisma.user.create({
+        data: {
+          provider: 'KAKAO',
+          providerId: 'test2',
+          email: 'test@test.com',
+          name: 'test2',
+          url: 'test2',
+        },
+      }),
+    ]);
+
+    const [feed, _, post] = await Promise.all([
+      prisma.feed.create({
+        data: {
+          authorId: user.id,
+          title: 'feed',
+          content: 'feed',
+          thumbnail: 'thumbnail',
+        },
+      }),
+      prisma.post.create({
+        data: {
+          authorId: user.id,
+          content: 'post',
+          title: 'post',
+          type: 1,
+        },
+      }),
+      prisma.post.create({
+        data: {
+          authorId: user2.id,
+          content: 'post',
+          title: 'post',
+          type: 1,
+        },
+      }),
+    ]);
+
+    await Promise.all([
+      prisma.postComment.create({
+        data: {
+          writerId: user.id,
+          postId: post.id,
+          content: 'comment',
+        },
+      }),
+      prisma.feedComment.create({
+        data: {
+          writerId: user.id,
+          feedId: feed.id,
+          content: 'comment',
+        },
+      }),
+    ]);
+
+    // when
+    const { status } = await request(app.getHttpServer())
+      .delete('/me')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send();
+
+    // then
+    expect(status).toBe(204);
+
+    const [userCount, feedCount, postCount, postCommentCount] =
+      await Promise.all([
+        prisma.user.count(),
+        prisma.feed.count(),
+        prisma.post.count(),
+        prisma.postComment.count(),
+      ]);
+
+    expect(userCount).toBe(1);
+    expect(feedCount).toBe(0);
+    expect(postCount).toBe(1);
+    expect(postCommentCount).toBe(1);
+  });
+});
