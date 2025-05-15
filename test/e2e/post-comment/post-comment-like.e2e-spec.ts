56cefd6978ffcb09a955ca11f03aa565579ec151
leemhoon00 <leemhoon000@gmail.com>
2025-05-15 17:58:48 +0900

    refactor: post-comment.repository에 httpException 로직 제거

--- test/e2e/post-comment/post-comment-like.e2e-spec.ts
+++ test/e2e/post-comment/post-comment-like.e2e-spec.ts
@@ -110,43 +110,4 @@ describe('PUT /post-comments/:id/like - 게시판 댓글 좋아요', () => {
     expect(afterComment.likeCount).toBe(1);
     expect(like.userId).toBe(user.id);
   });
-
-  it('이미 좋아요 했으면 409를 반환한다', async () => {
-    // given
-    const accessToken = await register(app, 'test');
-
-    const user = await prisma.user.findFirstOrThrow();
-    const post = await prisma.post.create({
-      data: {
-        authorId: user.id,
-        title: 'test',
-        content: 'test',
-        type: 1,
-      },
-    });
-
-    const comment = await prisma.postComment.create({
-      data: {
-        writerId: user.id,
-        postId: post.id,
-        content: 'test',
-      },
-    });
-
-    await request(app.getHttpServer())
-      .put(`/post-comments/${comment.id}/like`)
-      .set('Authorization', `Bearer ${accessToken}`)
-      .send();
-
-    // when
-    const { status } = await request(app.getHttpServer())
-      .put(`/post-comments/${comment.id}/like`)
-      .set('Authorization', `Bearer ${accessToken}`)
-      .send();
-
-    // then
-    expect(status).toBe(409);
-    const afterComment = await prisma.postComment.findFirstOrThrow();
-    expect(afterComment.likeCount).toBe(1);
-  });
 });
