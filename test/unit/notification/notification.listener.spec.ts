35cfb1a4fd9c42c855007970dec6b5fd09266314
leemhoon00 <leemhoon000@gmail.com>
2025-05-21 16:59:13 +0900

    test: FEED_LIKE 이벤트 유닛 테스트

--- test/unit/notification/notification.listener.spec.ts
+++ test/unit/notification/notification.listener.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
 import { NotificationListener } from 'src/module/notification/notification.listener';
 import { PrismaModule } from 'src/database/prisma/prisma.module';
 import { PrismaService } from 'src/database/prisma/prisma.service';
@@ -6,16 +7,20 @@ import { PrismaService } from 'src/database/prisma/prisma.service';
 describe('NotificationListener', () => {
   let prisma: PrismaService;
   let notificationListener: NotificationListener;
+  let app: INestApplication;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [PrismaModule],
-      providers: [PrismaService, NotificationListener],
+      providers: [NotificationListener],
     }).compile();
 
     prisma = module.get<PrismaService>(PrismaService);
     notificationListener =
       module.get<NotificationListener>(NotificationListener);
+
+    app = module.createNestApplication();
+    await app.init();
   });
 
   afterEach(async () => {
@@ -99,6 +104,88 @@ describe('NotificationListener', () => {
       const notification = await prisma.notification.findFirst({
         where: { userId: targetUser.id },
       });
+      expect(notification).toBeNull();
+    });
+  });
+
+  describe('FEED_LIKE 이벤트', () => {
+    it('유저가 구독했을 때 알림 데이터를 생성한다', async () => {
+      // given
+      const targetUser = await prisma.user.create({
+        data: {
+          provider: 'kakao',
+          providerId: 'test1',
+          url: 'test1',
+          name: 'test1',
+          image: 'test1',
+          email: 'test1@test.com',
+          subscription: ['FEED_LIKE'],
+        },
+      });
+
+      const feed = await prisma.feed.create({
+        data: {
+          authorId: targetUser.id,
+          title: 'test1',
+          thumbnail: 'test1',
+        },
+      });
+
+      // when
+      await notificationListener.handleFeedLikeEvent({
+        feedId: feed.id,
+        likeCount: 5,
+      });
+
+      // then
+      const notification = await prisma.notification.findFirstOrThrow({
+        where: { userId: targetUser.id },
+      });
+
+      expect(notification).toEqual({
+        id: expect.any(String),
+        userId: targetUser.id,
+        isRead: false,
+        createdAt: expect.any(Date),
+        link: `${process.env.SERVICE_URL}/feeds/${feed.id}`,
+        message: `${feed.title}에 좋아요가 5개 달렸어요`,
+        image: `${process.env.IMAGE_URL}/${feed.thumbnail}`,
+      });
+    });
+
+    it('유저가 구독하지 않았을 때 알림 데이터를 생성하지 않는다', async () => {
+      // given
+      const targetUser = await prisma.user.create({
+        data: {
+          provider: 'kakao',
+          providerId: 'test1',
+          url: 'test1',
+          name: 'test1',
+          image: 'test1',
+          email: 'test1@test.com',
+          subscription: [],
+        },
+      });
+
+      const feed = await prisma.feed.create({
+        data: {
+          authorId: targetUser.id,
+          title: 'test1',
+          thumbnail: 'test1',
+        },
+      });
+
+      // when
+      await notificationListener.handleFeedLikeEvent({
+        feedId: feed.id,
+        likeCount: 5,
+      });
+
+      // then
+      const notification = await prisma.notification.findFirst({
+        where: { userId: targetUser.id },
+      });
+
       expect(notification).toBeNull();
     });
   });
