a0f249467017cbac83568249204fd07e14322349
leemhoon00 <leemhoon000@gmail.com>
2025-05-22 14:50:03 +0900

    fix: 인기 랭킹 3일 -> 7일로 변경

--- src/module/feed/repository/feed.select.repository.ts
+++ src/module/feed/repository/feed.select.repository.ts
@@ -303,7 +303,7 @@ export class FeedSelectRepository {
       },
       where: {
         createdAt: {
-          gte: new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * 3),
+          gte: new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * 7),
         },
       },
       orderBy: {
