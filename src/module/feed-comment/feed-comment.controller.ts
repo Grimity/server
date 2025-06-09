f8b4ef1dfd27a9eb8db35d5c31969952b631e9f5
leemhoon00 <leemhoon000@gmail.com>
2025-06-09 16:57:38 +0900

    refactor: 피드 댓글 조회 v2 deprecate 처리

--- src/module/feed-comment/feed-comment.controller.ts
+++ src/module/feed-comment/feed-comment.controller.ts
@@ -57,7 +57,10 @@ export class FeedCommentController {
   }
 
   @ApiBearerAuth()
-  @ApiOperation({ summary: '피드 댓글 조회 v2 - Optional Guard' })
+  @ApiOperation({
+    summary: '피드 댓글 조회 v2 - Optional Guard',
+    deprecated: true,
+  })
   @ApiResponse({ status: 200, type: [ParentFeedCommentResponse] })
   @UseGuards(OptionalJwtGuard)
   @Get('v2')
@@ -69,33 +72,15 @@ export class FeedCommentController {
   }
 
   @ApiBearerAuth()
-  @ApiOperation({
-    summary: '피드 최상위 댓글 조회 - Optional Guard',
-    deprecated: true,
-  })
-  @ApiResponse({ status: 200, type: FeedCommentsResponse })
+  @ApiOperation({ summary: '피드 댓글 조회 - Optional Guard' })
+  @ApiResponse({ status: 200, type: [ParentFeedCommentResponse] })
   @UseGuards(OptionalJwtGuard)
   @Get()
   async findAll(
     @CurrentUser() userId: string | null,
     @Query() { feedId }: GetFeedCommentRequest,
-  ): Promise<FeedCommentsResponse> {
-    return await this.feedCommentService.getAllParentsByFeedId(userId, feedId);
-  }
-
-  @ApiBearerAuth()
-  @ApiOperation({
-    summary: '피드 자식 댓글 조회 - Optional Guard',
-    deprecated: true,
-  })
-  @ApiResponse({ status: 200, type: [FeedChildCommentResponse] })
-  @UseGuards(OptionalJwtGuard)
-  @Get('child-comments')
-  async findChildComments(
-    @CurrentUser() userId: string | null,
-    @Query() query: GetChildFeedCommentRequest,
-  ): Promise<FeedChildCommentResponse[]> {
-    return await this.feedCommentService.getChildComments(userId, query);
+  ): Promise<ParentFeedCommentResponse[]> {
+    return await this.feedCommentService.getComments(userId, feedId);
   }
 
   @ApiBearerAuth()
