65a48c316196875e72bd5ec043d253d53ea5e547
leemhoon00 <leemhoon000@gmail.com>
2025-06-24 01:29:26 +0900

    feat: socket.io & redis-adapter 세팅

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './common/config/swagger.config';
+import { RedisIoAdapter } from './database/redis/redis.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,5 +15,9 @@ async function bootstrap() {
   setupSwagger(app);
 
   await app.listen(process.env.PORT ?? 3000);
+
+  const redisIoAdapter = new RedisIoAdapter(app);
+  await redisIoAdapter.connectToRedis();
+  app.useWebSocketAdapter(redisIoAdapter);
 }
 bootstrap();
