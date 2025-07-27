9f844ae14cead65e7cd1fab109f61ea6044f7654
Seungtae Kim <25264932+kstash@users.noreply.github.com>
2025-07-27 20:47:31 +0900

    feat: e2e-spec 테스트 파일 추가

---
+++ test/e2e/chat/search-chats.e2e-spec.ts
@@ -0,0 +1,110 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import * as request from 'supertest';
+import { AppModule } from 'src/app.module';
+import { PrismaService } from 'src/database/prisma/prisma.service';
+import { register } from '../helper/register';
+import { AuthService } from 'src/module/auth/auth.service';
+import { sampleUuid } from '../helper/sample-uuid';
+
+describe('GET /chats/search - 채팅 검색(이름만)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+  let authService: AuthService;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = module.createNestApplication();
+    prisma = module.get<PrismaService>(PrismaService);
+    authService = module.get<AuthService>(AuthService);
+
+    jest.spyOn(authService, 'getKakaoProfile').mockResolvedValue({
+      kakaoId: 'test',
+      email: 'test@test.com',
+    });
+
+    await app.init();
+  });
+
+  afterEach(async () => {
+    await prisma.user.deleteMany();
+    await prisma.chat.deleteMany();
+    await prisma.chatUser.deleteMany();
+    await prisma.chatMessage.deleteMany();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('accessToken이 없을 때 401을 반환한다', async () => {
+    // when
+    const { status } = await request(app.getHttpServer())
+      .get('/chats/search')
+      .send();
+    // then
+    expect(status).toBe(401);
+  });
+
+  it('200과 함께 이름 검색 결과를 반환한다', async () => {
+    // given
+    const accessToken = await register(app, 'test');
+    const me = await prisma.user.findFirstOrThrow();
+    console.log(`me: ${JSON.stringify(me, null, 4)}`);
+
+    const user = await prisma.user.create({
+      data: {
+        provider: 'KAKAO',
+        providerId: 'test2',
+        name: 'test2',
+        email: 'test2@example.com',
+        url: 'test2',
+      },
+    });
+
+    await prisma.chat.create({
+      data: { id: sampleUuid },
+    });
+
+    await prisma.chatUser.createMany({
+      data: [
+        { userId: me.id, chatId: sampleUuid, enteredAt: new Date() },
+        { userId: user.id, chatId: sampleUuid, enteredAt: new Date() },
+      ],
+    });
+
+    await prisma.chatMessage.createMany({
+      data: [
+        {
+          chatId: sampleUuid,
+          userId: me.id,
+          content: 'Hello',
+          createdAt: new Date(),
+        },
+        {
+          chatId: sampleUuid,
+          userId: user.id,
+          content: 'Hello back',
+          createdAt: new Date(),
+        },
+      ],
+    });
+
+    // when
+    const { body, status } = await request(app.getHttpServer())
+      .get('/chats/search')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .query({
+        username: 'test2',
+        size: 10,
+      });
+    // then
+    expect(status).toBe(200);
+    expect(body.chats.length).toBe(1);
+    expect(body.chats[0].opponent.name).toBe('test2');
+    expect(body.chats[0].opponent.id).toBe(user.id);
+  });
+});
