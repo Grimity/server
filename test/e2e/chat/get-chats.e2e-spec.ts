eb709e7c7dbf67d17c810fb825b3b2d357be69b7
Seungtae Kim <25264932+kstash@users.noreply.github.com>
2025-08-05 23:25:44 +0900

    fix[e2e]: 채팅방 목록 조회 테스트 코드 cursor & username & size 값 사용편

--- test/e2e/chat/get-chats.e2e-spec.ts
+++ test/e2e/chat/get-chats.e2e-spec.ts
@@ -5,7 +5,7 @@ import { AppModule } from 'src/app.module';
 import { PrismaService } from 'src/database/prisma/prisma.service';
 import { register } from '../helper/register';
 import { AuthService } from 'src/module/auth/auth.service';
-import { sampleUuid } from '../helper/sample-uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 describe('GET /chats - 채팅 검색(이름만)', () => {
   let app: INestApplication;
@@ -53,59 +53,100 @@ describe('GET /chats - 채팅 검색(이름만)', () => {
     const accessToken = await register(app, 'test');
     const me = await prisma.user.findFirstOrThrow();
 
-    const user = await prisma.user.create({
-      data: {
-        provider: 'KAKAO',
-        providerId: 'test2',
-        name: 'test2',
-        email: 'test2@example.com',
-        url: 'test2',
-      },
-    });
+    const testUserCount = 50;
 
-    await prisma.chat.create({
-      data: { id: sampleUuid },
-    });
-
-    await prisma.chatUser.createMany({
-      data: [
-        { userId: me.id, chatId: sampleUuid, enteredAt: new Date() },
-        { userId: user.id, chatId: sampleUuid, enteredAt: new Date() },
-      ],
-    });
-
-    await prisma.chatMessage.createMany({
-      data: [
-        {
-          chatId: sampleUuid,
-          userId: me.id,
-          content: 'Hello',
-          createdAt: new Date(),
+    for (let i = 0; i < testUserCount; i++) {
+      const testUser = await prisma.user.create({
+        data: {
+          provider: 'KAKAO',
+          providerId: `test${i}`,
+          name: `test${i}`,
+          email: `test${i}@example.com`,
+          url: `test${i}`,
+          image: `https://test${i}.com/image.png`,
         },
-        {
-          chatId: sampleUuid,
-          userId: user.id,
-          content: 'Hello back',
-          createdAt: new Date(),
-        },
-      ],
-    });
-
-    // when
-    const { body, status } = await request(app.getHttpServer())
-      .get('/chats')
-      .set('Authorization', `Bearer ${accessToken}`)
-      .query({
-        username: 'test2',
-        size: 10,
       });
 
-    // then
-    expect(status).toBe(200);
-    expect(body.chats.length).toBe(1);
-    expect(body.chats[0].id).toBe(sampleUuid);
-    expect(body.chats[0].lastMessage.content).toBe('Hello back');
-    expect(body.chats[0].opponent.name).toBe('test2');
-    expect(body.chats[0].opponent.id).toBe(user.id);
+      const chat = await prisma.chat.create({
+        data: { id: uuidv4(), createdAt: new Date() },
+      });
+
+      await prisma.chatUser.createMany({
+        data: [
+          {
+            userId: me.id,
+            chatId: chat.id,
+            enteredAt: new Date(),
+            unreadCount: 1,
+          },
+          {
+            userId: testUser.id,
+            chatId: chat.id,
+            enteredAt: new Date(),
+            unreadCount: 1,
+          },
+        ],
+      });
+
+      await prisma.chatMessage.createMany({
+        data: [
+          {
+            chatId: chat.id,
+            userId: testUser.id,
+            content: `This is test message ${i} from testUser`,
+            createdAt: new Date(),
+          },
+          {
+            chatId: chat.id,
+            userId: me.id,
+            content: `This is test message ${i} from me`,
+            createdAt: new Date(),
+          },
+        ],
+      });
+    }
+
+    let cursor: string | null = null;
+    while (true) {
+      // when
+      const size = 5;
+      const { body, status } = await request(app.getHttpServer())
+        .get('/chats')
+        .set('Authorization', `Bearer ${accessToken}`)
+        .query({
+          username: 'test1',
+          size,
+          cursor,
+        });
+
+      cursor = body.nextCursor;
+
+      // then
+      expect(status).toBe(200);
+      // test1, test10 ~ test19 총 11개
+      expect(body.chats.length).toBeLessThanOrEqual(size);
+      expect(body.nextCursor).toBeDefined();
+
+      for (const [i, chat] of body.chats.entries()) {
+        expect(chat.id).toBeDefined();
+        expect(chat.opponent.id).toBeDefined();
+        expect(chat.opponent.name).toBeDefined();
+        expect(chat.opponent.image).toBeDefined();
+        expect(chat.opponent.url).toBeDefined();
+        expect(chat.lastMessage).toBeDefined();
+
+        // 마지막으로 온 채팅 메시지 순서대로 정렬되어 있는지 확인
+        if (i < body.chats.length - 1) {
+          expect(
+            new Date(chat.lastMessage.createdAt).getTime(),
+          ).toBeGreaterThan(
+            new Date(body.chats[i + 1].lastMessage.createdAt).getTime(),
+          );
+        }
+      }
+      if (!cursor) {
+        break;
+      }
+    }
   });
 });
