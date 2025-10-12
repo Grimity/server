427b9b3391c1afb14ff65c0ec15654772ebc9c5f
leemhoon00 <leemhoon000@gmail.com>
2025-10-12 14:43:05 +0900

    fix: 에러 로깅 추가

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './common/config/swagger.config';
 
+// 전역 에러 핸들러 추가
+process.on('uncaughtException', (error) => {
+  console.error('Uncaught Exception:', error);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+  // Promise rejection으로 인한 서버 다운 방지
+});
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
